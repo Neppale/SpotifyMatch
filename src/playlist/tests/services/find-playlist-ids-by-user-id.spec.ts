16c573488a2408b5508679139d7dc0e55f9e96d0
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2023-02-05 04:28:35 -0300

    feat: added tests to find playlist ids by user id service

---
+++ src/playlist/tests/services/find-playlist-ids-by-user-id.spec.ts
@@ -0,0 +1,58 @@
+import { GetAccessTokenServiceSpy } from '../../../utils/auth/tests/spy/services/get-access-token.service.spy';
+import { FindPlaylistIdsByUserIdService } from '../../../playlist/services/find-playlist-ids-by-user-id.service';
+import axios from 'axios';
+
+type SutOutput = {
+  sut: FindPlaylistIdsByUserIdService;
+  getAccessTokenServiceSpy: GetAccessTokenServiceSpy;
+};
+
+const makeSut = (): SutOutput => {
+  const getAccessTokenServiceSpy = new GetAccessTokenServiceSpy();
+  const sut = new FindPlaylistIdsByUserIdService(getAccessTokenServiceSpy);
+  return {
+    sut,
+    getAccessTokenServiceSpy,
+  };
+};
+
+describe('FindPlaylistIdsByUserIdService', () => {
+  it('should call getAccessTokenService.get once', async () => {
+    const { sut, getAccessTokenServiceSpy } = makeSut();
+    // how to mock axios.get?
+    jest.spyOn(axios, 'get').mockImplementationOnce(() => {
+      return Promise.resolve({
+        data: {
+          items: [
+            {
+              tracks: {
+                href: 'https://api.spotify.com/v1/playlists/playlistId/tracks',
+              },
+            },
+          ],
+        },
+      });
+    });
+    await sut.find('id');
+    expect(getAccessTokenServiceSpy.count).toBe(1);
+  });
+
+  it('should return an array of playlist ids', async () => {
+    const { sut } = makeSut();
+    jest.spyOn(axios, 'get').mockImplementationOnce(() => {
+      return Promise.resolve({
+        data: {
+          items: [
+            {
+              tracks: {
+                href: 'https://api.spotify.com/v1/playlists/playlistId/tracks',
+              },
+            },
+          ],
+        },
+      });
+    });
+    const playlistIds = await sut.find('id');
+    expect(playlistIds).toEqual(['playlistId']);
+  });
+});
---
+++ src/utils/auth/tests/spy/services/get-access-token.service.spy.ts
@@ -0,0 +1,12 @@
+import { GetAccessTokenService } from 'src/utils/auth/services/get-access-token.service';
+
+export class GetAccessTokenServiceSpy implements GetAccessTokenService {
+  clientSecret: string;
+  clientId: string;
+  accessToken = 'accessToken';
+  count = 0;
+  async get(): Promise<string> {
+    this.count++;
+    return Promise.resolve(this.accessToken);
+  }
+}
