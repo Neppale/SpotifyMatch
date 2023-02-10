505c532d51e1c916cbd959a141f0275d4c2f50d3
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2023-02-10 01:16:35 -0300

    fix: raised rate limit

--- src/profile/app.module.ts
+++ src/profile/app.module.ts
@@ -14,7 +14,7 @@ import { APP_GUARD } from '@nestjs/core/constants';
   imports: [
     ThrottlerModule.forRoot({
       ttl: 60,
-      limit: 3,
+      limit: 10,
     }),
   ],
   controllers: [ProfileController],
