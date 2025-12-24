0b6b9d1761c035d239ea7a1e7df5e86f3f88af3f
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2025-12-24 08:13:01 -0300

    chore: update Prisma output path and adjust import paths for better module resolution

--- src/prisma/services/prisma.service.ts
+++ src/prisma/services/prisma.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client/extension';
 import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3';
+import { PrismaClient } from '@PrismaClient';
 
 @Injectable()
 export class PrismaService implements OnModuleInit, OnModuleDestroy {
--- src/tracks/repositories/track.repository.ts
+++ src/tracks/repositories/track.repository.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '@Prisma/services/prisma.service';
-import { Prisma } from 'prisma/generated';
+import { Prisma } from '@PrismaClient';
 
 @Injectable()
 export class TrackRepository {
