b6f515371105bbae24ea5d0e68144337d59c4d2f
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2025-12-25 04:26:02 -0300

    refactor: update dependencies, remove cache handling, and clean up module imports

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,19 +1,12 @@
-import { CacheModule, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { ProfileController } from '@Profile/profile.controller';
 import { ProfileService } from '@Profile/services/profile.service';
 import { TrackModule } from '@Tracks/track.module';
-import { CacheService } from '@Utils/cache/services/cache.service';
 
 @Module({
-  imports: [
-    TrackModule,
-    CacheModule.register({
-      ttl: 60,
-      max: 100,
-    }),
-  ],
+  imports: [TrackModule],
   controllers: [ProfileController],
-  providers: [ProfileService, CacheService],
+  providers: [ProfileService],
   exports: [ProfileService],
 })
 export class ProfileModule {}
