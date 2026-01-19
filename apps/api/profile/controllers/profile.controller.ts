1a0c706827dc0694d88e7e0b8ba4e42b8d5c3128
Gabriel Verissimo Dias <gabrielverissimodias@gmail.com>
2026-01-19 08:19:58 +0000

    refactor: move observable creation to its own method in event emitter

--- apps/api/profile/controllers/profile.controller.ts
+++ apps/api/profile/controllers/profile.controller.ts
@@ -12,7 +12,6 @@ import { ProfileService } from '@Apps/api/profile/services/profile.service';
 import { Response } from 'express';
 import { TrackProcessingEventEmitter } from '@Apps/track-processing/services/track-processing-event-emitter.service';
 import { Observable } from 'rxjs';
-import { ProfileComparisonFormattedResponse } from '@Apps/shared/profile/models/profile-comparison.model';
 import { SpotifyAvailabilityGuard } from '@Utils/guards/spotify-availability.guard';
 import { SessionId } from '@Utils/decorators/session-id.decorator';
 
@@ -45,39 +44,6 @@ export class ProfileController {
   getProcessingStatus(
     @Param('sessionId') sessionId: string,
   ): Observable<MessageEvent> {
-    return new Observable<MessageEvent>((observer) => {
-      const listener = (data: {
-        success?: boolean;
-        error?: string;
-        progress?: number;
-        data?: ProfileComparisonFormattedResponse;
-      }) => {
-        if (data.error) {
-          observer.error(data.error);
-          observer.complete();
-        }
-        if (data.progress) {
-          observer.next({
-            data: JSON.stringify({ progress: data.progress }),
-          } as MessageEvent);
-        }
-        if (data.success) {
-          observer.next({ data: JSON.stringify(data.data) } as MessageEvent);
-          observer.complete();
-        }
-      };
-
-      this.eventEmitter.onForSession('success', sessionId, listener);
-      this.eventEmitter.onForSession('progress', sessionId, listener);
-      this.eventEmitter.onForSession('completed', sessionId, listener);
-      this.eventEmitter.onForSession('error', sessionId, listener);
-
-      return () => {
-        this.eventEmitter.offForSession('success', sessionId, listener);
-        this.eventEmitter.offForSession('progress', sessionId, listener);
-        this.eventEmitter.offForSession('completed', sessionId, listener);
-        this.eventEmitter.offForSession('error', sessionId, listener);
-      };
-    });
+    return this.eventEmitter.createObservable(sessionId);
   }
 }
--- apps/track-processing/services/track-processing-event-emitter.service.ts
+++ apps/track-processing/services/track-processing-event-emitter.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { EventEmitter } from 'events';
+import { Observable } from 'rxjs';
+import { ProfileComparisonFormattedResponse } from '@Apps/shared/profile/models/profile-comparison.model';
 
 @Injectable()
 export class TrackProcessingEventEmitter extends EventEmitter {
@@ -7,7 +9,7 @@ export class TrackProcessingEventEmitter extends EventEmitter {
     return super.emit(`${event}:${sessionId}`, data);
   }
 
-  onForSession(
+  private onForSession(
     event: string,
     sessionId: string,
     listener: (...args: any[]) => void,
@@ -15,11 +17,48 @@ export class TrackProcessingEventEmitter extends EventEmitter {
     return super.on(`${event}:${sessionId}`, listener);
   }
 
-  offForSession(
+  private offForSession(
     event: string,
     sessionId: string,
     listener: (...args: any[]) => void,
   ): this {
     return super.off(`${event}:${sessionId}`, listener);
+  }
+
+  createObservable(sessionId: string): Observable<MessageEvent> {
+    return new Observable<MessageEvent>((observer) => {
+      const listener = (data: {
+        success?: boolean;
+        error?: string;
+        progress?: number;
+        data?: ProfileComparisonFormattedResponse;
+      }) => {
+        if (data.error) {
+          observer.error(data.error);
+          observer.complete();
+        }
+        if (data.progress) {
+          observer.next({
+            data: JSON.stringify({ progress: data.progress }),
+          } as MessageEvent);
+        }
+        if (data.success) {
+          observer.next({ data: JSON.stringify(data.data) } as MessageEvent);
+          observer.complete();
+        }
+      };
+
+      this.onForSession('success', sessionId, listener);
+      this.onForSession('progress', sessionId, listener);
+      this.onForSession('completed', sessionId, listener);
+      this.onForSession('error', sessionId, listener);
+
+      return () => {
+        this.offForSession('success', sessionId, listener);
+        this.offForSession('progress', sessionId, listener);
+        this.offForSession('completed', sessionId, listener);
+        this.offForSession('error', sessionId, listener);
+      };
+    });
   }
 }
